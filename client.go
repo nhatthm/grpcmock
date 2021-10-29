bdca09ba98b2d4390b86f93d112c8494f1ec47c5
nhatthm <nt@hellofresh.com>
2021-10-30 00:00:44 +0200

    Add InvokeServerStream

--- client.go
+++ client.go
@@ -2,9 +2,12 @@ package grpcmock
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"net"
 	"net/url"
+	"reflect"
 	"strings"
 
 	"google.golang.org/grpc"
@@ -32,19 +35,63 @@ func InvokeUnary(
 	out interface{},
 	opts ...InvokeOption,
 ) error {
+	ctx, conn, method, callOpts, err := prepInvoke(ctx, method, opts...)
+	if err != nil {
+		return err
+	}
+
+	return conn.Invoke(ctx, method, in, out, callOpts...)
+}
+
+// InvokeServerStream invokes a server-stream method.
+func InvokeServerStream(
+	ctx context.Context,
+	method string,
+	in interface{},
+	handle func(stream grpc.ClientStream) error,
+	opts ...InvokeOption,
+) error {
+	ctx, conn, method, callOpts, err := prepInvoke(ctx, method, opts...)
+	if err != nil {
+		return err
+	}
+
+	desc := &grpc.StreamDesc{ServerStreams: true}
+
+	stream, err := conn.NewStream(ctx, desc, method, callOpts...)
+	if err != nil {
+		return err
+	}
+
+	if err := stream.SendMsg(in); err != nil {
+		return err
+	}
+
+	if err := stream.CloseSend(); err != nil {
+		return err
+	}
+
+	if handle == nil {
+		return nil
+	}
+
+	return handle(stream)
+}
+
+func prepInvoke(ctx context.Context, method string, opts ...InvokeOption) (context.Context, *grpc.ClientConn, string, []grpc.CallOption, error) {
 	addr, method, err := parseMethod(method)
 	if err != nil {
-		return fmt.Errorf("coulld not parse method url: %w", err)
+		return ctx, nil, "", nil, fmt.Errorf("coulld not parse method url: %w", err)
 	}
 
 	ctx, dialOpts, callOpts := invokeOptions(ctx, opts...)
 
 	conn, err := grpc.DialContext(ctx, addr, dialOpts...)
 	if err != nil {
-		return err
+		return ctx, nil, "", nil, err
 	}
 
-	return conn.Invoke(ctx, method, in, out, callOpts...)
+	return ctx, conn, method, callOpts, err
 }
 
 func parseMethod(method string) (string, string, error) {
@@ -135,4 +182,67 @@ func WithCallOption(opts ...grpc.CallOption) InvokeOption {
 	return func(c *invokeConfig) {
 		c.callOpts = append(c.callOpts, opts...)
 	}
+}
+
+// RecvAll reads everything from the stream and put into the output.
+func RecvAll(out interface{}) func(stream grpc.ClientStream) error {
+	return func(stream grpc.ClientStream) error {
+		typeOfPtr := reflect.TypeOf(out)
+
+		if typeOfPtr == nil || typeOfPtr.Kind() != reflect.Ptr {
+			return fmt.Errorf("%T is not a pointer", out) // nolint: goerr113
+		}
+
+		typeOfSlice := typeOfPtr.Elem()
+
+		if typeOfSlice.Kind() != reflect.Slice {
+			return fmt.Errorf("%T is not a slice", out) // nolint: goerr113
+		}
+
+		typeOfMsg := typeOfSlice.Elem()
+		newValueOf := reflect.MakeSlice(typeOfSlice, 0, 0)
+
+		for {
+			msg := newMessageValue(typeOfMsg)
+			err := stream.RecvMsg(msg.Interface())
+
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			if err != nil {
+				return fmt.Errorf("could not recv msg: %w", err)
+			}
+
+			newValueOf = appendMessage(newValueOf, msg.Elem())
+		}
+
+		reflect.ValueOf(out).Elem().Set(newValueOf)
+
+		return nil
+	}
+}
+
+func newMessageValue(t reflect.Type) reflect.Value {
+	if t.Kind() == reflect.Ptr {
+		return newMessageValue(t.Elem())
+	}
+
+	return reflect.New(t)
+}
+
+func newSliceMessageValue(t reflect.Type, v reflect.Value) reflect.Value {
+	if t.Kind() != reflect.Ptr {
+		return v
+	}
+
+	result := reflect.New(t.Elem())
+
+	result.Elem().Set(newSliceMessageValue(t.Elem(), v))
+
+	return result
+}
+
+func appendMessage(s reflect.Value, v reflect.Value) reflect.Value {
+	return reflect.Append(s, newSliceMessageValue(s.Type().Elem(), v))
 }
--- client_test.go
+++ client_test.go
@@ -3,12 +3,15 @@ package grpcmock_test
 import (
 	"context"
 	"errors"
+	"io"
 	"net"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/test/bufconn"
 
 	"github.com/nhatthm/grpcmock"
@@ -105,4 +108,128 @@ func TestInvokeUnary_Success(t *testing.T) {
 	grpcmock.MessageEqual(t, expectedRequest, actualRequest)
 	grpcmock.MessageEqual(t, expectedResponse, actualResponse)
 	assert.NoError(t, err)
+}
+
+func TestInvokeServerStream_DialError(t *testing.T) {
+	t.Parallel()
+
+	dialer := func(context.Context, string) (net.Conn, error) {
+		return nil, errors.New("dial error")
+	}
+
+	err := grpcmock.InvokeServerStream(context.Background(), "NotFound", nil, nil,
+		grpcmock.WithContextDialer(dialer),
+		grpcmock.WithInsecure(),
+	)
+	expected := `rpc error: code = Unavailable desc = connection error: desc = "transport: Error while dialing dial error"`
+
+	assert.EqualError(t, err, expected)
+}
+
+func TestInvokeServerStream_UnaryMethod(t *testing.T) {
+	t.Parallel()
+
+	item := &grpctest.Item{Id: 42}
+
+	dialer := testSrv.StartServer(t, testSrv.GetItem(func(context.Context, *grpctest.GetItemRequest) (*grpctest.Item, error) {
+		return item, nil
+	}))
+
+	err := grpcmock.InvokeServerStream(context.Background(),
+		"grpctest.ItemService/GetItem",
+		&grpctest.ListItemsRequest{},
+		func(stream grpc.ClientStream) error {
+			msg := &grpctest.Item{}
+			err := stream.RecvMsg(msg)
+
+			grpcmock.MessageEqual(t, item, msg)
+			assert.NoError(t, err)
+
+			// Stream is closed.
+			err = stream.RecvMsg(msg)
+			assert.ErrorIs(t, err, io.EOF)
+
+			return nil
+		},
+		grpcmock.WithContextDialer(dialer),
+		grpcmock.WithInsecure(),
+		grpcmock.WithHeader("Locale", "en-US"),
+	)
+
+	assert.NoError(t, err)
+}
+
+func TestInvokeServerStream_Success(t *testing.T) {
+	t.Parallel()
+
+	items := []*grpctest.Item{
+		{
+			Id:   41,
+			Name: "Item #41",
+		},
+		{
+			Id:   42,
+			Name: "Item #42",
+		},
+		{
+			Id:   43,
+			Name: "Item #43",
+		},
+	}
+
+	dialer := testSrv.StartServer(t, testSrv.ListItems(func(_ *grpctest.ListItemsRequest, server grpctest.ItemService_ListItemsServer) error {
+		var locale string
+
+		if md, ok := metadata.FromIncomingContext(server.Context()); ok {
+			if values := md.Get("locale"); len(values) > 0 {
+				locale = values[0]
+			}
+		}
+
+		for _, i := range items {
+			i.Locale = locale
+
+			if err := server.Send(i); err != nil {
+				return status.Error(codes.Internal, err.Error())
+			}
+		}
+
+		return nil
+	}))
+
+	result := make([]*grpctest.Item, 0)
+
+	err := grpcmock.InvokeServerStream(context.Background(),
+		"grpctest.ItemService/ListItems",
+		&grpctest.ListItemsRequest{},
+		grpcmock.RecvAll(&result),
+		grpcmock.WithContextDialer(dialer),
+		grpcmock.WithInsecure(),
+		grpcmock.WithHeader("Locale", "en-US"),
+	)
+
+	expected := []*grpctest.Item{
+		{
+			Id:     41,
+			Locale: "en-US",
+			Name:   "Item #41",
+		},
+		{
+			Id:     42,
+			Locale: "en-US",
+			Name:   "Item #42",
+		},
+		{
+			Id:     43,
+			Locale: "en-US",
+			Name:   "Item #43",
+		},
+	}
+
+	assert.NoError(t, err)
+	assert.Equal(t, len(expected), len(result))
+
+	for i := 0; i < len(expected); i++ {
+		grpcmock.MessageEqual(t, expected[i], result[i])
+	}
 }
